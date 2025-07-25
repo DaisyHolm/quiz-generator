db0be47955cd4cfaf8eafc6ea70226fd33ada63b
DaisyHolm <daisyholm16@gmail.com>
2025-07-25 08:13:49 +0200

    Added the last media queries

--- src/components/MyStoredQuizzes/MyStoredQuizzes.jsx
+++ src/components/MyStoredQuizzes/MyStoredQuizzes.jsx
@@ -6,49 +6,55 @@ import "./MyStoredQuizzes.css";
 export default function MyStoredQuizzes() {
   const { quizzesList } = useContext(MyQuizzesContext);
 
-  if (!quizzesList || Object.keys(quizzesList).length === 0) {
-    return <p>No quizzes Found</p>;
-  }
-
   return (
     <>
       <div className="my-quizzes-container">
         <div className="my-quizzes">
           <div className="my-quizzes-header">
             <h2>My Quizzes</h2>
-            <Button path="/create-quiz">Create new Quiz</Button>
+            <Button path="/create-quiz">Create New Quiz</Button>
           </div>
           <div className="quiz-list">
-            {Object.entries(quizzesList).map(([id, value]) => {
-              console.log(value, "What's the value?");
-              return (
-                <div className="quiz-list-item" key={id}>
-                  <div className="quiz-title">
-                    <h3>{value.title}</h3>
-                  </div>
-                  <div className="quiz-description">
-                    <p>{value.description}</p>
-                  </div>
-                  <div className="quiz-btns">
-                    <Button
-                      className="btn-small"
-                      path={`/edit-quiz/${id}`}
-                      quiz={value}
-                    >
-                      Edit
-                    </Button>
-                    {value.questions.length === 0 ? null : (
-                      <Button className="btn-small" path={`/play/${id}`}>
-                        Play
+            {Object.keys(quizzesList).length === 0 ? (
+              <div className="no-quizzes-saved">
+                <p className="crying-emoji">(╥‸╥)</p>
+                <p>
+                  You have no saved quizzes at the moment. Create one to get
+                  started!
+                </p>
+              </div>
+            ) : (
+              Object.entries(quizzesList).map(([id, value]) => {
+                console.log(value, "What's the value?");
+                return (
+                  <div className="quiz-list-item" key={id}>
+                    <div className="quiz-title">
+                      <h3>{value.title}</h3>
+                    </div>
+                    <div className="quiz-description">
+                      <p>{value.description}</p>
+                    </div>
+                    <div className="quiz-btns">
+                      <Button
+                        className="btn-small"
+                        path={`/edit-quiz/${id}`}
+                        quiz={value}
+                      >
+                        Edit
                       </Button>
-                    )}
+                      {value.questions.length === 0 ? null : (
+                        <Button className="btn-small" path={`/play/${id}`}>
+                          Play
+                        </Button>
+                      )}
+                    </div>
                   </div>
-                </div>
-              );
-            })}
+                );
+              })
+            )}
           </div>
           <div className="quiz-list-main-menu">
-            <Button path="/">Back to main menu</Button>
+            <Button path="/">Back To Main Menu</Button>
           </div>
         </div>
       </div>
